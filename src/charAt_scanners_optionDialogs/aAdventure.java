c5cdf2b30de1b7b03c9b010360bfe1a67e22e8c0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-03-20 16:58:34 -0700

    module 4 level 0

--- src/charAt_scanners_optionDialogs/aAdventure.java
+++ src/charAt_scanners_optionDialogs/aAdventure.java
@@ -19,8 +19,21 @@ if (task1==0) {
 				new String[] { "Go to a yogurt place", "Go to shore", "Go home" }, null);
 		if (taskBeach==1) {
 			JOptionPane.showMessageDialog(null, "You get many waves!");
-			JOptionPane.showOptionDialog(null, "What do you want to do?", "Fun!", 1, JOptionPane.INFORMATION_MESSAGE, null,
+			int task2=JOptionPane.showOptionDialog(null, "What do you want to do?", "Fun!", 1, JOptionPane.INFORMATION_MESSAGE, null,
 					new String[] { "Go to shore", "Go home", "Go to yogurt place" }, null);
+			if (task2==0) {
+				JOptionPane.showMessageDialog(null, "You arrive on the shore and you want to do something chill.");
+				if (task2==1) {
+					JOptionPane.showMessageDialog(null, "You go home after a long day and you get a little tired as you rest your body on your bed...");
+				}
+				if (task2==2) {
+					JOptionPane.showMessageDialog(null, "You head to the yogurt place");
+				}
+			} 
+			if (taskBeach==2) {
+				JOptionPane.showMessageDialog(null, "After a long day you start to dose off...");
+			}
+			
 		}
 	}
 	
@@ -30,7 +43,21 @@ if (task1==1) {
 	int taskMall=JOptionPane.showOptionDialog(null, "What do you want to do?", "Fun!", 1, JOptionPane.INFORMATION_MESSAGE, null,
 			new String[] { "Go eat something", "Go shop", "Go home" }, null);
 	if (taskMall==0) {
-		
+		JOptionPane.showMessageDialog(null, "The mall has many shops and resterants!");
+				int taskShop=JOptionPane.showOptionDialog(null, "Where do you want to go?", "Fun!", 1, JOptionPane.INFORMATION_MESSAGE, null,
+						new String[] { "Bobby's Hamburger Shop", "Target", "Go home" }, null);
+				if (taskShop==0) {
+					JOptionPane.showMessageDialog(null, "Good choice! The burgers are great there!");
+				}
+				if (taskShop==1) {
+					JOptionPane.showMessageDialog(null, "Good choice! You walk into target!");
+				}
+				if (taskShop==2) {
+					JOptionPane.showMessageDialog(null, "You go home after a long day and you start to fall asleep...");
+				}
+				
+				
+				
 	}
 	
 	
@@ -38,5 +65,6 @@ if (task1==1) {
 if (task1==2) {
 	JOptionPane.showMessageDialog(null, "You decide to stay at home. Have some good sleep!");
 }
+
 }
 }
